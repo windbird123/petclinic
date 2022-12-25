d0ec0c9a8baa2a017b8e3de0f5a41429b110fe80
jaemyung.kim <jaemyung.kim@navercorp.com>
2022-12-25 21:59:23 +0900

    dev

--- backend/src/main/scala/petclinic/Main.scala
+++ backend/src/main/scala/petclinic/Main.scala
@@ -3,14 +3,18 @@ package petclinic
 import org.postgresql.ds.PGSimpleDataSource
 import petclinic.server.{ClinicServer, PetRoutes, RootRoutes}
 import petclinic.services.PetServiceLive
-import zio.http.Server
+import zio.http.{Server, ServerConfig}
 import zio.{Task, ULayer, ZIO, ZIOAppDefault, ZLayer}
 
+import java.net.InetSocketAddress
 import javax.sql.DataSource
 
 object Main extends ZIOAppDefault {
   val dataSourceLayer: ULayer[DataSource] = ZLayer.succeed(new PGSimpleDataSource)
+
+  val myServer = ZLayer.succeed(ServerConfig(address = new InetSocketAddress(8081))) >>> Server.live
   override def run: Task[Unit] = ZIO
     .serviceWithZIO[ClinicServer](_.start)
-    .provide(Server.default, ClinicServer.layer, PetRoutes.layer, RootRoutes.layer, PetServiceLive.layer, dataSourceLayer)
+    .provide(myServer, ClinicServer.layer, PetRoutes.layer, RootRoutes.layer, PetServiceLive.layer, dataSourceLayer)
+//    .provide(Server.default, ClinicServer.layer, PetRoutes.layer, RootRoutes.layer, PetServiceLive.layer, dataSourceLayer)
 }
--- backend/src/main/scala/petclinic/server/RootRoutes.scala
+++ backend/src/main/scala/petclinic/server/RootRoutes.scala
@@ -10,17 +10,18 @@ import zio.json._
 import java.io.File
 
 final case class RootRoutes() {
-  val routes: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
-
-    // Gets a single Pet found by their parsed ID and returns it as JSON.
-    case Method.GET -> "" /: path =>
-      ZIO.succeed(Response.text(s"Hello 2 [$path]"))
-  }
-
-//  val routes: Http[Any, Throwable, Request, Response] = Http.collectHttp[Request] {
-//    case Method.GET ->  "" /: path =>
-//      Http.fromFile(new File("application.conf"))
+//  val routes: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
+//
+//    // Gets a single Pet found by their parsed ID and returns it as JSON.
+//    case Method.GET -> "" /: path =>
+//      ZIO.succeed(Response.text(s"Hello 2 [$path]"))
 //  }
+
+  val routes: Http[Any, Throwable, Request, Response] = Http.collectHttp[Request] {
+    case Method.GET -> "" /: path =>
+      println(path)
+      Http.fromFile(new File(s"../dist/$path"))
+  }
 }
 
 object RootRoutes {
